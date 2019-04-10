0c3743ccf94840d577728c8f27c95d6a88d37643
ogbk <ogbu.k.olu@gmail.com>
2019-04-10 13:38:52 +0100

    books fetching, display, preview -- OK

--- app/components/App.js
+++ app/components/App.js
@@ -1,124 +1,104 @@
 // @flow
 
 import React, { Component } from 'react';
-// import { Preview } from './Preview';
+import { Preview } from './Preview';
 
-// import listItems from '../../utils/garment_items.json';
-
-export type Article = {
-  product_categories_mapped: Array<number>,
-  product_id: string,
-  url: string,
-  gender: string,
-  brand: string,
-  product_description: string,
-  image_urls: Array<string>,
-  product_imgs_src: Array<string>,
-  source: string,
-  product_categories: Array<string>,
-  images: Array<{
-    url: string,
-    path: string,
-    checksum: string,
-  }>,
-  price: string,
-  product_title: string,
+export type Book = {
+  id: string,
+  book_author: Array<string>,
+  book_pages: number,
+  book_publication_city: string,
+  book_publication_country: string,
+  book_publication_year: number,
+  book_title: string,
 };
 
 type State = {
   searchKey: string,
-  listItems: Array<Article>,
-  selectedItem: Article,
-  selectedItemId: string,
-  matchedItems: Array<Article>,
-  itemClicked: boolean;
+  listBooks: Array<Book>,
+  selectedBook: Book,
+  selectedBookId: string,
+  matchedBooks: Array<Book>,
+  bookClicked: boolean;
 };
 
 export class App extends Component<{}, State> {
-  searchItem: (any) => void;
-  selectItem:(item: Article) => void;
+  // searchBook: (any) => void;
+  selectBook: (book: Book) => void;
 
   constructor() {
     super();
 
     this.state = {
       searchKey: '',
-      listItems: [],
-      selectedItem: {},
-      selectedItemId: '',
-      matchedItems: [],
-      itemClicked: false,
+      listBooks: [],
+      selectedBook: {},
+      selectedBookId: '',
+      matchedBooks: [],
+      bookClicked: false,
     };
 
-    // this.searchItem = this.searchItem.bind(this);
-    // this.selectItem = this.selectItem.bind(this);
+    // this.searchBook = this.searchBook.bind(this);
+    this.selectBook = this.selectBook.bind(this);
   }
 
   componentDidMount() {
     fetch('http://nyx.vima.ekt.gr:3000/api/books', {
       method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
     })
       .then(res => res.json())
-      .then(res => console.log(res))
-      .catch(err => console.log(`Request failed: ${err}`));
+      .then((res) => {
+        this.setState({
+          listBooks: res.books,
+          matchedBooks: res.books,
+        });
+      })
+      .catch(err => console.log(`Request failed--: ${err}`));
   }
 
   /*
-  searchItem(evt: any): void {
+  searchBook(evt: any): void {
     const searchKey = evt.target.value.toLowerCase();
-    const { listItems: items } = this.state;
-    const matchedItems = items.filter(
+    const { listBooks: books } = this.state;
+    const matchedBooks = books.filter(
       ({ product_title }) => (product_title.toLowerCase().includes(searchKey)),
     );
 
     this.setState({
       searchKey,
-      matchedItems,
-    });
-  }
-
-  selectItem(item: Article): void {
-    const { product_id } = item;
-
-    this.setState({
-      selectedItem: item,
-      selectedItemId: product_id,
-      itemClicked: true,
+      matchedBooks,
     });
   }
   */
+
+  selectBook(book: Book): void {
+    const { id } = book;
+
+    this.setState({
+      selectedBook: book,
+      selectedBookId: id,
+      bookClicked: true,
+    });
+  }
+
   render() {
-    // const { selectedItem, selectedItemId, itemClicked, matchedItems } = this.state;
+    const { selectedBook, selectedBookId, bookClicked, matchedBooks } = this.state;
 
-    return (<div>EMPTY</div>
-      /*
+    return (
       <div className="app">
-        <div className="search-bar">
-          <span>Filter item by product title &nbsp; : &nbsp; &nbsp;</span>
-          <input
-            type="text"
-            className={matchedItems.length ? 'input-ok' : 'input-error'}
-            onChange={this.searchItem}
-          />
-        </div>
-
         <div className="items">
           <div className="items-list">
             <table>
               <tbody>
                 {
-                  matchedItems
+                  matchedBooks
                     .map(item => (
                       <tr
-                        key={`tr_${item.product_id}`}
-                        className={item.product_id === selectedItemId ? 'click clicked-row' : 'click'}
-                        onClick={() => this.selectItem(item)}
+                        key={`tr_${item.id}`}
+                        className={item.id === selectedBookId ? 'click clicked-row' : 'click'}
+                        onClick={() => this.selectBook(item)}
                       >
-                        <td key={`td_${item.product_id}`}> {item.product_title} </td>
+                        <td key={`td_${item.id}`}> {item.book_title} </td>
                       </tr>
                     ))
                 }
@@ -128,13 +108,12 @@ export class App extends Component<{}, State> {
 
           <div className="item-details">
             {
-              itemClicked &&
-              <Preview item={selectedItem} />
+              bookClicked &&
+              <Preview book={selectedBook} />
             }
           </div>
         </div>
       </div>
-      */
     );
   }
 }
--- app/components/Preview.js
+++ app/components/Preview.js
@@ -1,23 +1,19 @@
 // @flow
 
 import React from 'react';
-import type { Article } from './App';
+import type { Book } from './App';
 
-const defaultImgSrc = '../public/img/image-not-found.jpg';
-
-export const Preview = ({ item }: {item: Article}) => {
-  const { product_id, product_title, brand, price, images } = item;
+export const Preview = ({ book }: {book: Book}) => {
+  const {
+    id,
+    book_author,
+    book_title,
+  } = book;
   return (
     <div>
-      <span>{`Product ID  ---  ${product_id}`}</span><br />
-      <span>{`Title  ---  ${product_title}`}</span><br />
-      <span>{`Brand       ---  ${brand}`}</span><br />
-      <span>{`Price       ---  ${price}`}</span><br />
-      <img
-        src={images[0].url}
-        alt={product_id}
-        onError={(e) => { e.target.src = defaultImgSrc; }}
-      />
+      <span>{`Book ID  ---  ${id}`}</span><br />
+      <span>{`Author  ---  ${book_author}`}</span><br />
+      <span>{`Title       ---  ${book_title}`}</span><br />
     </div>
   );
 };
